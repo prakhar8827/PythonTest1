4936a6cf4d0c4319e25118540ebeba4e690f70e3
Prakhar Pandey <prakhar.pandey011@gmail.com>
2022-11-21 19:45:58 +0530

    First Commit

---
+++ Day1/Comments.py
@@ -0,0 +1,17 @@
+#name: Prakhar
+#application : e-commerce ctrl+/
+import keyword
+
+print("i am working on python")
+
+"""this is python program  ctrl shift /
+working on pycharm ide
+which comes from jetbrain"""
+
+'''this is python program
+working on pycharm ide
+which comes from jetbrain'''
+
+# dfffff
+
+print(keyword.kwlist)
