c617f431c45f0d61e6aea1be9f9c097cbe8390f5
prakhar877078 <prakharpandey73@gmail.com>
2022-11-24 19:59:53 +0530

    Day 2 Python Concept

---
+++ Day2/Operators.py
@@ -0,0 +1,36 @@
+#Arithmetic operators
+# a=5
+# b=2
+# print(a+b)  # addition of a , b
+# print(a-b) # substration
+# print(a*b) # multiplication
+# print(a/b)   # 2.5
+# print(a//b)   # 2 quotient
+# print(a%b)  # 1 remider
+# print(a**b)  # 5**2 = 25
+
+# Relational operators/comparison operators
+# > <  >=  <=  !=  ==
+# Relational operators always returns boolean value True/False
+# a=5
+# b=10
+#
+# print(a>b)  # False
+# print(a<b) # True
+# print(a==b) # False
+# print(a!=b) # True
+# print(a<=b)  # True
+# print(a>=b) # False
+
+#Logical operators
+#  and  or  not
+#always returns boolean value True/False
+a=True
+b=False
+
+print(a and b) # False
+print(a or b) # True
+print(not a) # False
+
+
+
